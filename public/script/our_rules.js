cbcdc25a60c3993993194b5dc83259cce1a8e297
Cinquain <james@cityxcape.com>
2019-10-02 23:34:29 -0400

    finished layout for rules

---
+++ public/script/our_rules.js
@@ -0,0 +1,15 @@
+const box1 = document.getElementById('box1');
+const box2 = document.getElementById('box2');
+const box3 = document.getElementById('box3');
+
+box1.addEventListener('click', () => {
+    location.href = "https://www.youtube.com/watch?v=WkkC9cK8Hz0"
+});
+
+box2.addEventListener('click', () => {
+    location.href = ""
+});
+
+box3.addEventListener('click', () => {
+    location.href = ""
+})
