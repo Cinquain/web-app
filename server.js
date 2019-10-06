6eab9971f829b66c03740240567210e4813a9def
Cinquain <james@cityxcape.com>
2019-10-06 18:28:38 -0400

    created signup endpoint

--- server.js
+++ server.js
@@ -14,3 +14,13 @@ app.listen(Port, () => {
 app.get('/', (req, res) => {
     res.render('index.html')
 })
+
+app.post('/new_signup', (req, res, err) => {
+    console.log('Attempting to create new signup');
+
+    if (err) {
+        console.log('Error creating new user signup')
+    }
+    res.redirect('http://google.com');
+    res.end()
+})
