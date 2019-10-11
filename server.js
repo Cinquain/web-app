296bcf40b0303b72bfc71776164e2f0f74bbc20c
Cinquain <james@cityxcape.com>
2019-10-10 23:54:40 -0400

    added body-parser and signup endpoint

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 
 const Port = 3008
+
 app.use(morgan('short'));
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static('public'))
 
 app.listen(Port, () => {
@@ -16,11 +19,17 @@ app.get('/', (req, res) => {
 })
 
 app.post('/new_signup', (req, res, err) => {
-    console.log('Attempting to create new signup');
+
+    var name = req.body.fullName 
+    var email = req.body.email
+    var city = req.body.city
+
+    console.log(name, email, city);
 
     if (err) {
-        console.log('Error creating new user signup')
+        console.log('Error creating new user signup', err)
     }
+
     res.redirect('http://google.com');
     res.end()
 })
