963ea4fd512ecc23a58903f3f527ac7937ea2c32
Cinquain <james@cityxcape.com>
2019-10-20 11:47:55 -0400

    fixed home page responsiveness

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static('public'))
 
 app.listen(PORT, () => {
-    console.log('Server is up and running on: ')
+    console.log('Server is up and running on 3008')
 })
 
 const pool = mysql.createConnection({
